0adb9985fa2da558992d961549b23eb58674f295
Zoe Ji <zoe.q.ji@gmail.com>
2017-09-04 17:34:24 -0700

    fixed window scrolling with up/down keys

--- public/script.js
+++ public/script.js
@@ -332,6 +332,12 @@ $(function() {
     sendMessage();
     scrollChat();
   });
+  window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+  }, false);
 })
 
 function scrollChat() {
